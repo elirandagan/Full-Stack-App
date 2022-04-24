74525655e9dbe4c7cd784ac921f127976d1c3a04
elirandagan <elirandagan123@gmail.com>
2022-04-24 22:40:05 +0300

    repair logout functionality

--- sce-2022/src/controllers/auth.ts
+++ sce-2022/src/controllers/auth.ts
@@ -15,8 +15,8 @@ const generateTokens = (userId: string): [string, string] => {
     process.env.REFRESH_TOKEN_SECRET,
     {}
   );
-  return [accessToken, refreshToken]
-}
+  return [accessToken, refreshToken];
+};
 /**
  * register
  * @param {http req} req
@@ -48,7 +48,7 @@ const register = async (req: Request, res: Response) => {
   try {
     const newUser = await user.save();
     //login - create access token
-    const [accessToken, refreshToken] = generateTokens(newUser._id)
+    const [accessToken, refreshToken] = generateTokens(newUser._id);
     newUser.refreshToken = refreshToken;
     await newUser.save();
     res.status(StatusCodes.OK).send({
@@ -92,7 +92,7 @@ const login = async (req: Request, res: Response) => {
         .send({ error: "wrong email or password" });
     }
 
-    const [accessToken, refreshToken] = generateTokens(user._id)
+    const [accessToken, refreshToken] = generateTokens(user._id);
     user.refreshToken = refreshToken;
     await user.save();
     res.status(StatusCodes.OK).send({
@@ -123,19 +123,19 @@ const renewToken = async (req: Request, res: Response) => {
   jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async (err, userId) => {
     console.log("jwt.verify");
     if (err != null) {
-      return res.status(StatusCodes.FORBIDDEN).send({ err: err.message })
+      return res.status(StatusCodes.FORBIDDEN).send({ err: err.message });
     }
     try {
-      const id: string = userId['_id']
+      const id: string = userId["_id"];
       const user2 = await User.findById(id);
       if (user2.refreshToken != token) {
         user2.refreshToken = "";
         await user2.save();
-        console.log("refresh token not valid - not present in DB")
+        console.log("refresh token not valid - not present in DB");
         return res.status(StatusCodes.FORBIDDEN).send({ error: err.message });
       }
 
-      const [accessToken, refreshToken] = generateTokens(id)
+      const [accessToken, refreshToken] = generateTokens(id);
       user2.refreshToken = refreshToken;
       await user2.save();
       console.log("StatusCodes.OK");
@@ -155,14 +155,18 @@ const renewToken = async (req: Request, res: Response) => {
  * @param {http req} req
  * @param {http res} res
  */
- const logout = async (req: Request, res: Response) => {
+const logout = async (req: Request, res: Response) => {
   console.log("logout");
   const user = await User.findOne({ email: req.body.email });
-  user.refreshToken = ' '
-  await user.save()
-  res.status(StatusCodes.OK).json('you have been logout.')
- };
-
+  if (!user) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send({ error: "can't find the user for logging out" });
+  }
+  user.refreshToken = " ";
+  await user.save();
+  res.status(StatusCodes.OK).json("you have been succesfuly loged out.");
+};
 
 export = {
   register,
